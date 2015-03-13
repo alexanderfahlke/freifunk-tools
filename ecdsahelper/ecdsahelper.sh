b649db2d749fb0cb0ae2d25c3cd66b7e1e8fddd4
Alexander Fahlke <alexander@fahlke.org>
2015-03-13 09:43:00 +0100

    added more checks for the key and build directories

--- ecdsahelper/ecdsahelper.sh
+++ ecdsahelper/ecdsahelper.sh
@@ -15,19 +15,38 @@
 # limitations under the License.
 
 # adjust the following two lines (if you need)
-KEY_DIR="${HOME}/.ssh"
-KEY_FILENAME='freifunkfw'
+KEY_DIR="${HOME}/.freifunkkeystore" # final output directory for the keys
+KEY_FILENAME='freifunkfw' # filename of the keys
 
 # do not edit anything below this line!
-BUILD_DIR=$(mktemp -dt "ecdsahelper.XXXXXX")
+
+# variables for the key file names
 PRIVATE_KEY_FILENAME="${KEY_DIR}/${KEY_FILENAME}.ecdsa"
 PUBLIC_KEY_FILENAME="${KEY_DIR}/${KEY_FILENAME}.ecdsa.pub"
 
-if [ -e "${PRIVATE_KEY_FILENAME}" ] && [ -f "${PRIVATE_KEY_FILENAME}" ]
-then
-  echo "Warning, the keyfile ${KEY_DIR}/${KEY_FILE_NAME}.ecdsa already exists"
-  echo "Aborting!"
-  exit 1
+# try to create the build temp directory
+BUILD_TMP_DIR=$(mktemp -q -dt 'ecdsahelper')
+if [ $? -ne 0 ]; then
+	echo "$0: Can't create temp directory, exiting..."
+	exit 1
+fi
+
+# exit if KEY_DIR is a file
+if [ -e "${KEY_DIR}" ] && [ -f "${KEY_DIR}" ]; then
+  echo "$0: The keystore ${KEY_DIR} is not a directory, exiting..."
+  exit 2
+fi
+
+# create KEY_DIR if it does not exist
+if [ ! -e "${KEY_DIR}" ] && [ ! -d "${KEY_DIR}" ]; then
+	mkdir "${KEY_DIR}"
+	echo '*' > "${KEY_DIR}/.gitignore" # add gitignore file to prevent adding the keys to a git repository ;)
+fi
+
+# exit if private key already exists
+if [ -e "${PRIVATE_KEY_FILENAME}" ] && [ -f "${PRIVATE_KEY_FILENAME}" ]; then
+  echo "$0: The private key ${KEY_DIR}/${KEY_FILE_NAME}.ecdsa already exists, exiting..."
+  exit 3
 fi
 
 # prerequisites for the build
@@ -36,7 +55,7 @@ sudo apt-get install -y git pkg-config cmake
 sudo apt-get install -y doxygen
 
 # make and install libuecc
-cd "${BUILD_DIR}"
+cd "${BUILD_TMP_DIR}"
 wget 'http://git.universe-factory.net/libuecc/snapshot/libuecc-4.tar'
 tar xvf 'libuecc-4.tar'
 cd 'libuecc-4'
@@ -46,7 +65,7 @@ cmake ..
 sudo make install
 
 # make and install ecdsakeygen
-cd "${BUILD_DIR}"
+cd "${BUILD_TMP_DIR}"
 git clone 'https://github.com/tcatm/ecdsautils.git'
 cd 'ecdsautils'
 mkdir 'build'
@@ -58,3 +77,10 @@ sudo make install
 ecdsakeygen -s > "${PRIVATE_KEY_FILENAME}"
 ecdsakeygen -p < "${PRIVATE_KEY_FILENAME}" > "${PUBLIC_KEY_FILENAME}"
 chmod 400 "${PRIVATE_KEY_FILENAME}" # writing intentionally disabled ;)
+
+# clean up build directory
+if [ -n "${BUILD_TMP_DIR}" ] && [ "${BUILD_TMP_DIR}" != "/" ]; then
+	if [ -e "${BUILD_TMP_DIR}" ] && [ -d "${BUILD_TMP_DIR}" ]; then
+		rm -rf "${BUILD_TMP_DIR}"
+	fi
+fi
